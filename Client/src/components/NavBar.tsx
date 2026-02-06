b93b7ce73401cde81abe92c7727ca963ecabc092
Shamik Zaffar <affableshamik98@gmail.com>
2026-02-07 00:31:09 +0530

    navbar refactor

--- Client/src/components/NavBar.tsx
+++ Client/src/components/NavBar.tsx
@@ -1,77 +1,116 @@
 import { useEffect, useRef, useState } from "react";
-import { motion } from "motion/react";
-import { useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "motion/react";
+import { NavLink, useLocation } from "react-router-dom";
 
-import NavLinks from "./NavLinks.js";
 import Button from "@components/common/Button";
-import useModalStore from "@/stores/modalStore";
-import { Menu, Search, SquareChevronDown } from "lucide-react";
+import { Menu, Minimize2, Search, Stethoscope } from "lucide-react";
+import useAuthStore from "@/stores/authStore";
+
+const navLinks = [
+  { label: "Home", route: "/" },
+  { label: "Find", route: "/" },
+  { label: "Ask", route: "/chat" },
+];
 
 const NavBar = () => {
   const { pathname = "" } = useLocation();
-  const [showRoutes, setShowRoutes] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const user = useAuthStore((s) => s.user);
 
   useEffect(() => {
-    if (showRoutes) setShowRoutes(false);
+    setShowMobileMenu(false);
   }, [pathname]);
 
   const ref = useRef<HTMLElement>(null);
 
   useEffect(() => {
     const navbar = ref?.current as HTMLElement;
+
     const handleMouseDown = (e: MouseEvent) => {
-      if (navbar && showRoutes && !navbar.contains(e.target as Node))
-        setShowRoutes(false);
+      if (showMobileMenu && !navbar?.contains(e.target as Node)) {
+        setShowMobileMenu(false);
+      }
     };
 
-    document.addEventListener("click", handleMouseDown);
-    return () => document.removeEventListener("click", handleMouseDown);
-  }, [showRoutes]);
-
-  useEffect(() => {
-    const handleEscape = (e: KeyboardEvent) => {
-      if (showRoutes && e.key == "Escape") setShowRoutes(false);
+    const handleKeydown = (e: KeyboardEvent) => {
+      if (showMobileMenu && e.key == "Escape") {
+        setShowMobileMenu(false);
+      }
     };
 
-    document.addEventListener("keydown", handleEscape);
-    return () => document.removeEventListener("keydown", handleEscape);
-  }, [showRoutes]);
+    document.addEventListener("keydown", handleKeydown);
+    document.addEventListener("mousedown", handleMouseDown);
 
-  function showAuthOptions() {
-    useModalStore.getState().openModal("authOptions", {
-      position: "top",
-    });
-  }
+    return () => {
+      document.removeEventListener("keydown", handleKeydown);
+      document.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, [showMobileMenu]);
 
   return (
-    <motion.nav
-      ref={ref}
-      className="container flex min-h-14 justify-between items-center relative"
-    >
-      <Button
-        variant="icon"
-        className="lg:hidden"
-        onClick={() => setShowRoutes((p) => !p)}
-      >
-        <Menu />
-        <span className="sr-only">Toggle menu</span>
-      </Button>
+    <motion.header className="shadow-xl shadow-slate-300/20 border-b-2 border-b-slate-300/40 p-8 py-6 relative">
+      <div className="max-w-7xl flex items-center justify-between mx-auto">
+        <Button variant="icon">
+          <Stethoscope />
+        </Button>
 
-      <div className="flex items-center gap-4 italic font-bold cursor-pointer lg:order-10">
-        <div className="flex items-center gap-2">
-          Ctrl K
-          <Button variant="icon">
+        {/* Large screen navigation */}
+        <nav className="hidden md:flex items-center gap-2">
+          {navLinks.map((navItem) => (
+            <NavLink key={navItem.label} to={navItem.route}>
+              {navItem.label}
+            </NavLink>
+          ))}
+        </nav>
+
+        <div className="flex items-center gap-6 font-bold cursor-pointer">
+          <Button className="italic gap-2" variant="icon">
+            Ctrl K
             <Search />
+          </Button>
+
+          <Button
+            variant="icon"
+            className="md:hidden"
+            onClick={() => setShowMobileMenu((p) => !p)}
+          >
+            {showMobileMenu ? <Minimize2 /> : <Menu />}
           </Button>
         </div>
 
-        <Button onClick={showAuthOptions} variant="icon">
-          <SquareChevronDown />
-        </Button>
+        {/* Mobile Navigation */}
+        <AnimatePresence>
+          {showMobileMenu && (
+            <motion.nav
+              ref={ref}
+              className="md:hidden absolute top-full left-0 w-full bg-white 
+              flex flex-col gap-6 shadow-lg p-8"
+              initial={{
+                y: -20,
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                opacity: 1,
+              }}
+              exit={{ y: -20, opacity: 0 }}
+            >
+              {navLinks.map((navItem) => (
+                <NavLink
+                  key={navItem.label}
+                  to={navItem.route}
+                  className="text-lg font-semibold"
+                >
+                  {navItem.label}
+                </NavLink>
+              ))}
+              <Button size="xl">Sign in</Button>
+            </motion.nav>
+          )}
+        </AnimatePresence>
       </div>
-
-      <NavLinks showRoutes={showRoutes} />
-    </motion.nav>
+    </motion.header>
   );
 };
 

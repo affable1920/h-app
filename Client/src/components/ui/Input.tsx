ee7f9767bc46e7c0a459e9c75979f44ca96e7285
Shamik <affableshamik98@gmail.com>
2026-04-04 11:13:26 +0530

    make the input ui component flexible by making it accept size props and adjust size based on the prop.

--- Client/src/components/ui/Input.tsx
+++ Client/src/components/ui/Input.tsx
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useMemo, type ComponentPropsWithoutRef } from "react";
 import Text from "./Label";
 
-interface AnimatedInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+type Size = "sm" | "md" | "lg";
+
+interface BaseProps {
   id?: string;
   label?: string;
   error?: string;
 }
 
-const Input = React.forwardRef<HTMLInputElement, AnimatedInputProps>(
-  ({ label, id, className, error, ...props }, ref) => {
+const sizes: Record<Size, string> = {
+  sm: `text-sm p-3`,
+  md: `p-4.5`,
+  lg: `text-md p-6`,
+};
+
+type InputProps = BaseProps & {
+  size?: Size;
+} & Omit<ComponentPropsWithoutRef<"input">, "size">;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, id, className, error, size = "md", ...props }, ref) => {
+    const base = `border-2 border-slate-200 rounded-md outline-none w-full font-semibold placeholder:italic
+            hover:border-secondary/40 hover:ring-2 transition-colors hover:ring-accent/20 focus:ring-2 focus:ring-accent/20 px-3 text-xs`;
+
+    const sz = sizes[size];
+
+    const config = useMemo(
+      function () {
+        const config = [base, sz];
+        return config.filter(Boolean).join(" ").trim();
+      },
+      [sz],
+    );
+
     return (
       <div className="flex flex-col gap-2">
         {label && (
-          <Text as="label" italic bold htmlFor={id}>
+          <Text as="label" italic bold htmlFor={id} className="px-3">
             {label}
           </Text>
         )}
 
         <div className="relative">
-          <input
-            id={id}
-            ref={ref}
-            {...props}
-            className={`border-2 border-slate-200 rounded-md outline-none w-full font-semibold placeholder:italic
-            hover:border-secondary/40 hover:ring-2 transition-colors hover:ring-accent/20 focus:ring-2 focus:ring-accent/20 p-3 text-xs ${className}`}
-          />
+          <input id={id} ref={ref} {...props} className={`${config}`} />
           {error && (
-            <Text italic bold className="mt-2 text-[9px] text-red-600!">
+            <Text
+              italic
+              bold
+              className="mt-2 text-[9px] line-clamp-1 text-red-600!"
+            >
               {error} !
             </Text>
           )}

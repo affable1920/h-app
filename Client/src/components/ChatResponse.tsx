08bb96d7dd43cb3de9d062031f5f0a21af32c155
Shamik <affableshamik98@gmail.com>
2026-06-21 14:31:56 +0530

    switch md library to use for model responses - from ReactMarkdown to streamdown which automatically handles most of the stuff react-markdown did using plugins and extra packages.

--- Client/src/components/ChatResponse.tsx
+++ Client/src/components/ChatResponse.tsx
@@ -1,75 +1,22 @@
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import { Streamdown } from "streamdown";
 import remarkBreaks from "remark-breaks";
-import { AnimatePresence, motion } from "motion/react";
 
-interface ResponseProps {
+function ChatResponse({
+  content,
+  stream = false,
+}: {
   content: string;
-  isStreaming: boolean;
-}
-
-function ChatResponse(response: ResponseProps) {
+  stream?: boolean;
+}) {
   return (
-    <article className="prose max-w-none last:inline text-text-secondary">
-      <div className="inline *:my-0">
-        <ReactMarkdown
-          components={{
-            a({ href, ...props }) {
-              return (
-                <a href={href} target="_blank">
-                  {props.children}
-                </a>
-              );
-            },
-            h1({ children, style, ...props }) {
-              return (
-                <h1
-                  style={{
-                    ...style,
-                    color: `var(--color-text-normal)`,
-                  }}
-                  {...props}
-                >
-                  {children}
-                </h1>
-              );
-            },
-            strong({ children, ...props }) {
-              return (
-                <strong
-                  style={{ ...props.style, color: "var(--color-text-normal" }}
-                  {...props}
-                >
-                  {children}
-                </strong>
-              );
-            },
-          }}
-          remarkPlugins={[remarkGfm, remarkBreaks]}
-        >
-          {response.content}
-        </ReactMarkdown>
-      </div>
-
-      <AnimatePresence>
-        {response.isStreaming && (
-          <motion.span
-            className="w-1.5 h-[1em] inline-block"
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: [1, 1, 0, 0],
-              transition: {
-                duration: 1,
-                repeat: Infinity,
-                repeatType: "loop",
-                ease: "linear",
-                times: [0, 0.5, 0.5, 1],
-              },
-            }}
-            exit={{ opacity: 0 }}
-          />
-        )}
-      </AnimatePresence>
+    <article>
+      <Streamdown
+        caret={"block"}
+        remarkPlugins={[remarkBreaks]}
+        isAnimating={stream}
+      >
+        {content}
+      </Streamdown>
     </article>
   );
 }
